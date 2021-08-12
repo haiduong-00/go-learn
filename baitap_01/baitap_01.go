dbd651e985e3e8567a8b9df43583c3077b691ebd
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-12 21:36:30 +0700

    Tải bài tập lên

---
+++ baitap_01/baitap_01.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	M := 9
+	N := 18
+	if M == N {
+		fmt.Println("UCLN", M)
+	 if   (M>n) , M=M-N ;
+	else N=N-M
+
+		
+		fmt.Println("UCLN",M)
+
+	}
+}
