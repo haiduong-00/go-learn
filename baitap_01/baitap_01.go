cbbf2e4301b93806201828cd39510e049f9cf09d
hehecoi222 <hehecoi@outlook.com>
2021-08-12 21:55:42 +0700

    Sửa lỗi

--- baitap_01/baitap_01.go
+++ baitap_01/baitap_01.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	M := 9
-	N := 18
-	if M == N {
-		fmt.Println("UCLN", M)
-	 if   (M>n) , M=M-N ;
-	else N=N-M
-
-		
-		fmt.Println("UCLN",M)
-
+	M := 1
+	N := 5
+	for M != N {
+		if M == N {
+			fmt.Println("UCLN", M)
+			break
+		} else if M > N {
+			M = M - N
+		} else {
+			N = N - M
+		}
 	}
+
+	fmt.Println("UCLN", M)
+
 }
