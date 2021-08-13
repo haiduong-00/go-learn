1e44663469361431f209a06b2b619f3b5a65abce
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-13 09:40:31 +0700

    Sửa bài tập tìm UCLN

--- baitap_01/baitap_01.go
+++ baitap_01/baitap_01.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	M := 9
+	M := 36
 	N := 18
-	if M == N {
-		fmt.Println("UCLN", M)
-	 if   (M>n) , M=M-N ;
-	else N=N-M
-
-		
-		fmt.Println("UCLN",M)
-
+	for M != N {
+		if M == N {
+			break
+		} else if M > N {
+			M = M - N
+		} else {
+			N = N - M
+		}
 	}
+	fmt.Println("UCLN cua M, N la", M)
 }
