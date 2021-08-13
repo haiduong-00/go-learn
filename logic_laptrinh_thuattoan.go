8bd8a2f638e7bc7984d1793b98ba48e0cd9d9599
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-13 11:09:34 +0700

    Logic

---
+++ logic_laptrinh_thuattoan.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("How are you today?")
+
+	//Hiểu vấn đề đang giải quyết
+	//S = 1/(a+0) + 1/(a+1) + ... + 1/(a+100)
+
+	//Phân tích và đưa ra hướng giải quyết (thuật toán) //Logic
+
+	//Sử dụng for để tạo số chạy tăng dần
+	// 1/(a+i)
+	//i:=0; i<=100; i++
+
+	// S = S + 1/(a+i)
+
+	//Thực hành
+	//Viết đúng syntax, statement
+	//Quy tắc của ngôn ngữ sử dụng
+
+	//If và else (else if) là 1 statement
+
+	//Kiểm thử
+	//Track từng biến, thuật toán xem có đúng như những gì mình xây dựng không
+	//Nếu máy báo lỗi: xem lại syntax, statement
+	//Nếu máy trả kết quả sai hoặc không chạy (không báo lỗi ngay) so với thuật toán đã xây dựng: xem lại thuật toán của đoạn code
+	//Nếu thuật toán trả kết quả sai so với đề bài (cách giải khác đã kiểm chứng là đúng) thì xây dựng lại thuật toán
+
+	//Khách hàng yêu cầu chương trình thiết kế đẹp
+	//Xây dựng thuật toán vẽ đẹp (thuật toán trong đoạn code phải ứng với thuật toán mình mong muốn, đã xây dựng)
+	//Đưa cho khách hàng xem đẹp hay xấu
+	//Nếu khách hàng bảo đẹp: Thuật toán tốt
+	//Nếu khách hàng chê: Xem lại cách xây dựng thuật toán
+
+}
