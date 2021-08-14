24218974375743adff72090a753cab68b3c2d6b3
hehecoi222 <hehecoi@outlook.com>
2021-08-15 01:36:41 +0700

    Fix to Original File

--- baitap_01/baitap_01.go
+++ baitap_01/baitap_01.go
@@ -3,24 +3,6 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	M := 9
-	N := 18
-	for M != N {
-=======
-	M := 15
-	N := 5
-	for {
->>>>>>> 0f15e8477303765884ed94bde54c3f9f92900f99
-		if M == N {
-			fmt.Println("UCLN", M)
-			break
-		} else if M > N {
-			M = M - N
-		} else {
-			N = N - M
-=======
 	for i := 10; i <= 21; i = i + 2 {
 		for i := 1; i <= 2; i++ {
 			fmt.Println(i * 30 * 120)
@@ -33,13 +15,6 @@ func main() {
 		}
 		for i := 1; i <= 3; i++ {
 			fmt.Println(i * 150 * 120)
->>>>>>> 0d3c324162bb59b22291c4fe010a6d03b5c67fa3
 		}
 	}
-<<<<<<< HEAD
-
-	fmt.Println("UCLN", M)
-
-=======
->>>>>>> 0f15e8477303765884ed94bde54c3f9f92900f99
 }
