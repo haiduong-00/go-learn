d7611cfb40a0267de1166af0c75093be89761aee
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-14 10:58:49 +0700

    change 01

---
+++ baitap_01/baitap_04/baitap_kethop_for_if.go
@@ -0,0 +1,27 @@
+//Cho một số a bất kì, a thuộc tập N*, thực hiện những điều sau trong vòng lặp
+//Nếu a là số chẵn, chia 2 cho chính nó và tiếp tục vòng lặp
+//Nếu a là số lẻ, nhân a với 3 và cộng thêm 1 và tiếp tục vòng lặp
+//Nếu a == 1 thì thoát khỏi vòng lặp
+//Cuối cùng in ra số lần lặp
+
+//for, a != 1, if, %:mod: chia lấy dư, a(biến), i :biến, nó có thể được truy cập ở ngoài vòng for
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 0
+	a := 22
+	for a != 1 {
+		if a%2 == 0 {
+			a = a / 2
+			i++
+		} else {
+			a = a*3 + 1
+			i++
+		}
+	}
+	fmt.Println(i)
+}
