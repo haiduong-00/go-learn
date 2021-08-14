4ea30fb67f8109d77da7ea712a05084c4322052b
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-14 22:46:00 +0700

    change3

--- baitap_01/baitap_01.go
+++ baitap_01/baitap_01.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func main() {
 	for i := 10; i <= 21; i = i + 2 {
-		for i := 10; i <= 11; i++ {
+		for i := 1; i <= 2; i++ {
 			fmt.Println(i * 30 * 120)
 		}
-		for i := 12; i <= 13; i++ {
+		for i := 1; i <= 2; i++ {
 			fmt.Println(i * 120 * 120)
 		}
-		for i := 14; i <= 18; i++ {
+		for i := 1; i <= 5; i++ {
 			fmt.Println(i * 40 * 120)
 		}
-		for i := 19; i <= 21; i++ {
+		for i := 1; i <= 3; i++ {
 			fmt.Println(i * 150 * 120)
 		}
 	}
