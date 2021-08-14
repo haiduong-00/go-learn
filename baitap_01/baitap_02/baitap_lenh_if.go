8f41d4ac71345f1a42ac3e55beeb091379d15e52
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-14 09:51:59 +0700

    change-02

--- baitap_01/baitap_02/baitap_lenh_if.go
+++ baitap_01/baitap_02/baitap_lenh_if.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 const favSport string = "football"
-const callout int = 2
+const callout int = 4
 
 var (
 	Jane int = 0
@@ -12,11 +12,16 @@ var (
 )
 
 func main() {
+	//Bài yêu cầu kiểm tra 1 trong 3 callout, nếu callout nào đúng thì in ra callout đấy
+	//if, 3 identifier, 2 constants,
+
 	if callout == Jane {
 		fmt.Println("Jane", favSport)
 	} else if callout == Josh {
 		fmt.Println("Josh", favSport)
-	} else {
+	} else if callout == Jihn {
 		fmt.Println("Jihn", favSport)
+	} else {
+		fmt.Println("ko có gì")
 	}
 }
