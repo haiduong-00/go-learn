a84d1eaf4ca02b11ef4426ce78536fe381a50f03
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-16 09:16:19 +0700

    change05

--- baitap_01/baitap_05.go/nhap.go
+++ baitap_01/baitap_05.go/nhap.go
@@ -6,15 +6,15 @@ func main() {
 	S := 0
 	for i := 9; i < 21; i++ {
 		if 9 <= i < 11 {
-			fmt.Println("S", S+i*30*120)
-		}else if 11 <= i < 13 ;i++ {
-			fmt.Println("S", S+i*120*120)
-		}else if 13 <= i < 18 {
-			fmt.Println("S", S+i*40*120)
-		}else if 18 <= i < 21 {
-			fmt.Println("S", S+i*150*120)
+			fmt.Println(S + i*30*120)
+		} else if 11 <= i < 13 {
+			fmt.Println(S + i*120*120)
+		} else if 13 <= i < 18 {
+			fmt.Println(S + i*40*120)
+		} else if 18 <= i < 21 {
+			fmt.Println(S + i*150*120)
 		}
-	} 
+	}
 	fmt.Println("S")
 
 }
