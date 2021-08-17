fc0405779ee5f5256606a455f944cc21575e2fd7
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-17 08:41:25 +0700

    change -2

--- baitap_05/cach2_BT.go
+++ baitap_05/cach2_BT.go
@@ -7,17 +7,17 @@ func main() {
 	S := 0
 	for i := 9; i < 21; i++ {
 		switch {
-		 	case (i >= 9) && (i < 11)
-			  {
+		case (i >= 9) && (i < 11):
+
 			S += 30 * sotien_1_khach_chi
-			} case (i >= 11) && (i < 13) 
-			{
+		case (i >= 11) && (i < 13):
 			S += 120 * sotien_1_khach_chi
-		}	case (i >= 13) && (i < 18) {
+		case (i >= 13) && (i < 18):
 			S += 40 * sotien_1_khach_chi
-		} 	case (i >= 18) && (i < 21) {
+		case (i >= 18) && (i < 21):
 			S += 150 * sotien_1_khach_chi
 		}
 	}
 
 	fmt.Println("Tổng doanh thu 1 ngày của nhà hàng là:", S, "nghìn vnđ")
+}
