767d45e080e41c86e8c8e888ac94938fcd18f118
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-18 11:03:41 +0700

    Học về array

---
+++ array.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	aint := []int{1, 2, 3}
+
+	
+	// string literal: một đoạn string gắn sẵn, và không thay đổi (optional)
+	// number literal
+	// Là một phần hằng số gắn sẵn, và nó không thay đổi (optional)
+	// aint[0][0] = 4  // indicies start from 0 to len(aint-1)
+	// aint[4] = 5
+	fmt.Println(aint)
+	// len(aint);
+
+	//Performance, Time
+	//Comptitive Programming: Lập trình thi đấu
+	//Slice: Go đã nhanh sẵn rồi
+
+	//Java, C++: ngôn ngữ ưu tiên tốc độ, performance
+	//Không có slice, chỉ có array
+
+	// Python, JS, R, Go: đều có slice (bởi vì ưu tiên độ dễ hiểu, dễ đọc, dễ học)
+	// Khackhang {
+	// 	name : "Hai Duong",
+	// 	age : "18",
+	// 	trinhdohocvan : "Tot nghiep cap 3"
+	// }
+}
