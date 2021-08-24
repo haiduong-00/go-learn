c39c6b0913fb2ab40abd1f4130aaaf8dfe10f527
haiduong-00 <dinhhaiduong203@gmail.com>
2021-08-24 10:09:09 +0700

    Xong bài struct

--- struct.go
+++ struct.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Non-anonymous struct
 
@@ -11,36 +13,43 @@ type person struct {
 }
 
 type Duong struct {
-	person  // Embedded type
+	person              // Embedded type
 	footballSkillRanked int
-	footballPassion bool
+	footballPassion     bool
+	fullName            string
 }
 
-
+type dog struct {
+	isDog bool
+}
 
 // type <tên kiểu> struct {
 // 		<tên đặc điểm> <kiểu dữ liệu>
 // 		<tên đặc điểm> <kiểu dữ liệu>
 // }
 
-
-func (t *Duong) fastSprint()  {
+func (t *Duong) fastSprint() { // Method & Pointer
 	fmt.Println("Dương đang dốc bóc rất nhanh đến khu vực đội bạn")
 }
 
-func (t *person) eat()  {
-	fmt.Printf("%v đang ăn", t.fullName)
+func (t *person) eat() {
+	fmt.Printf("%v đang ăn\n", t.fullName)
+}
+
+func (t *dog) eat() {
+	fmt.Println("Jake đang ăn")
 }
 
 func main() {
 	Duong := Duong{ // Object: Duong
-		person: person{
-			height: 178,
-			weight: 70,
+		person: person{ // Embedded struct
+			height:   178,
+			weight:   70,
 			fullName: "Đinh Hải Dương",
 		},
 		footballSkillRanked: 8,
-		footballPassion: true,
+		footballPassion:     true,
+		fullName:            "ABC",
 	}
 	fmt.Println(Duong.fullName)
 
@@ -73,4 +82,28 @@ func main() {
 	// }
 	// fmt.Println(map["Duong"])
 
+	Jake := struct {
+		dog  // Embedded struct
+		age  int
+		name string
+		// isDog bool
+	}{
+		dog: dog{
+			isDog: true,
+		},
+		age:  15,
+		name: "Jake",
+		// isDog: false,
+	}
+	// Anonymous struct so với OOP: (hỗ trợ -> có)
+	// field, data: hỗ trợ đầy đủ
+	// method, behavior: không hỗ trợ
+	// Encapsulation: không hỗ trợ (do không có method, behavior)
+	// Abstraction: chưa rõ
+	// Inheritance: hỗ trợ đầy đủ (cả data và method)
+	// Polymorphism: hỗ trợ 1 phần (chỉ data)
+
+	// Polymorphism chỉ có ở field (data)
+	Jake.eat()
+	fmt.Println(Jake)
 }
