74ece068ef8b2ae0f5e04407f85fdf91d8630f24
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-14 11:05:09 +0700

    chứng minh câu * sai

--- Channel/channel.go
+++ Channel/channel.go
@@ -3,32 +3,38 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Channel trong Concurrency
 // Channel: như một dây chuyền sản xuất
 // Có routine sản xuất, và có routine nhận (lấy) hàng ra
-// Các routine chỉ thực hiện 1 hướng: routine nào lấy hàng thì sẽ chỉ lấy, routine sản xuất thì chỉ sản xuất
+// Các routine chỉ thực hiện 1 hướng: routine nào lấy hàng thì sẽ chỉ lấy, routine sản xuất thì chỉ sản xuất (câu này sai)*
 // Những routine phụ có thể lấy, hoặc sản suất. Nhưng riêng routine main thì chỉ có NHẬN
 // Code bên dưới là 1 ví dụ về việc routine main không thể sản xuất
 // Khi sản xuất muốn thu hồi lại sản phẩm: buffered channel: queue
+// *Vì nếu có công việc thay ca, và sắp xếp việc sao cho channel có hàng: routine có thể thay ca nhau
 
 func main() {
-	c := make(chan int, 100)
+	c := make(chan int)
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(3)
 	go func() {
-		c <- 22
+		c <- 411
+		time.Sleep(time.Second*2)
+		fmt.Println(<-c)
 		wg.Done()
 	}()
 	go func() {
-		c <- 42
-		c <- 38
 		fmt.Println(<-c)
-		fmt.Println(<-c)
-		fmt.Println(<-c)
+		c <- 55
 		wg.Done()
 	}()
-	
+	go func() {
+		time.Sleep(time.Second)
+		fmt.Println(<-c)
+		c<-32
+		wg.Done()
+	}()
 	wg.Wait()
 }
