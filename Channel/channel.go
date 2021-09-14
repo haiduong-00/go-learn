2effb58fe61e3d777c2c1bac5dfc675a3b5b7562
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-14 09:51:12 +0700

    main không thể sản xuất

--- Channel/channel.go
+++ Channel/channel.go
@@ -8,18 +8,15 @@ import (
 // Channel: như một dây chuyền sản xuất
 // Có routine sản xuất, và có routine nhận (lấy) hàng ra
 // Các routine chỉ thực hiện 1 hướng: routine nào lấy hàng thì sẽ chỉ lấy, routine sản xuất thì chỉ sản xuất
+// Những routine phụ có thể lấy, hoặc sản suất. Nhưng riêng routine main thì chỉ có NHẬN
+// Code bên dưới là 1 ví dụ về việc routine main không thể sản xuất
 
 func main() {
 	c := make(chan int) //
 	quit := make(chan bool)
+	c <- 42
+	fmt.Println("Wait")
 	go func() {
-		c <- 32
-		c <- 48
-		c <- 56
-	}()
-	go func() {
-		fmt.Println(<-c)
-		fmt.Println(<-c)
 		fmt.Println(<-c)
 		quit <- true
 	}()
