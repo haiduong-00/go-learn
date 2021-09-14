7b903cb337781e985afaa236979d046739190b31
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-14 10:55:57 +0700

    Bufferef Channel

--- Channel/channel.go
+++ Channel/channel.go
@@ -11,27 +11,24 @@ import (
 // Các routine chỉ thực hiện 1 hướng: routine nào lấy hàng thì sẽ chỉ lấy, routine sản xuất thì chỉ sản xuất
 // Những routine phụ có thể lấy, hoặc sản suất. Nhưng riêng routine main thì chỉ có NHẬN
 // Code bên dưới là 1 ví dụ về việc routine main không thể sản xuất
+// Khi sản xuất muốn thu hồi lại sản phẩm: buffered channel: queue
 
 func main() {
-	c := make(chan int) //
+	c := make(chan int, 100)
 	var wg sync.WaitGroup
 	wg.Add(2)
 	go func() {
-		c <- 42
-		fmt.Println(<-c)  
+		c <- 22
 		wg.Done()
 	}()
 	go func() {
+		c <- 42
 		c <- 38
+		fmt.Println(<-c)
+		fmt.Println(<-c)
+		fmt.Println(<-c)
 		wg.Done()
 	}()
+	
 	wg.Wait()
 }
-
-
-// Ví dụ nhỏ về channel:
-// Login: nếu có xác thực 2 bước
-// Tất cả những thứ sau đây đều chạy Parallelism:
-// Trang web sẽ tạo code: sản xuất xong code rồi
-// Trang web khởi động hệ thống kiểm tra code: code mẫu, chỉ chờ code từ người dùng
-// Trang web tạo trang nhập code người dùng
