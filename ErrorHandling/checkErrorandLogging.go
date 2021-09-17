4f3f817e70776b66521c23c8ded3fb665d5f5b0d
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-17 10:13:30 +0700

    Các cách trả ra error

--- ErrorHandling/checkErrorandLogging.go
+++ ErrorHandling/checkErrorandLogging.go
@@ -5,6 +5,11 @@ import "fmt"
 // Practice: Thói quen tốt: Check lỗi liên tục (nhưng có vài trường hợp ngoại lệ)
 // Do error là 1 TYPE
 
+// Go hỗ trợ rất nhiều cách in ra lỗi:
+// fmt.Print(); log.Print() (log sẽ trả ra cả ngày giờ gặp lỗi cụ thể) (Dạng chỉ IN ra lỗi)
+// log.Panic(), panic() (Gây ra panicking, ảnh hưởng đến function và caller gọi ra function đó, theo hiệu ứng Domino)
+// log.Fatal(),exit() (Buộc dừng process ngay lập tức)
+
 func check(err error) {
 	if err != nil {
 		fmt.Println(err)
