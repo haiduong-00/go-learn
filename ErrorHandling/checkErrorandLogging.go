1360ffdb361e8291bef792fffcc51cff481d8d23
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-17 10:18:37 +0700

    Với fmt.Print()

--- ErrorHandling/checkErrorandLogging.go
+++ ErrorHandling/checkErrorandLogging.go
@@ -5,10 +5,10 @@ import "fmt"
 // Practice: Thói quen tốt: Check lỗi liên tục (nhưng có vài trường hợp ngoại lệ)
 // Do error là 1 TYPE
 
-// Go hỗ trợ rất nhiều cách in ra lỗi:
-// fmt.Print(); log.Print() (log sẽ trả ra cả ngày giờ gặp lỗi cụ thể) (Dạng chỉ IN ra lỗi)
+// Go hỗ trợ rất nhiều cách in ra lỗi (sắp xếp theo mức độ nghiêm trọng):
+// fmt.Print(); log.Print(), fmt.Errorf() (log sẽ trả ra cả ngày giờ gặp lỗi cụ thể) (Dạng chỉ IN ra lỗi)
 // log.Panic(), panic() (Gây ra panicking, ảnh hưởng đến function và caller gọi ra function đó, theo hiệu ứng Domino)
-// log.Fatal(),exit() (Buộc dừng process ngay lập tức)
+// log.Fatal()  (Buộc dừng process ngay lập tức)
 
 func check(err error) {
 	if err != nil {
@@ -17,8 +17,6 @@ func check(err error) {
 }
 
 func main() {
-	var y int
-	_, err := fmt.Scanln(&y) // Nếu y nhập vào không là số
+	err := fmt.Errorf("một lỗi nào đấy")
 	check(err)
-	fmt.Print(y) // Lệnh in này thường không cần check lỗi, nó chỉ có in giá trị bình thường ra
 }
