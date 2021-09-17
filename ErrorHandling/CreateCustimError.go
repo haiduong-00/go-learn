d3aef185be6e1f125c895b54dcdfcce2bf7dc4ce
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-17 11:12:12 +0700

    Cách tạo custom struct type error

---
+++ ErrorHandling/CreateCustimError.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Trong Go, trong ngôn ngữ khác: có thể tạo ra một error/exception riêng tùy ý
+// Trong Go: các cách tạo error:
+// fmt.Errorf(format string, a ...interface{}) error
+// errors.New(text string) error
+// Tạo 1 struct có method Error() string
+
+type err struct{
+	mainerr error
+	when time.Time
+}
+
+func (t err) Error() string  {
+	return fmt.Sprintf("An error name %v has occured at %v",t.mainerr,t.when)
+}
+
+func main()  {
+	err := err{
+		mainerr: fmt.Errorf("Lỗi"),
+		when: time.Now(),
+	}
+	panic(err)
+}
